93d3d49dd6433f297ddc8b547e95e2ea2b2e4fba
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2025-03-03 14:24:07 +0000

    Add project data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -4,31 +4,20 @@ interface Project {
   href?: string
   imgSrc?: string
 }
-interface Project {
-  title: string
-  description: string
-  href?: string
-  imgSrc?: string
-}
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Cargo & Aviation Visual Anomaly Detection',
+    description: `
+      At Visiocue GmbH, based in Berlin and proudly funded by the Beumer Group, we are dedicated to revolutionizing cargo and aviation logistics. Our innovative AI solutions harness state-of-the-art computer vision techniques along with LiDar technology and wireless cameras to detect bad luggage that could potentially disrupt conveyor belt mechanics. This pioneering approach not only enhances operational safety but also optimizes the overall cargo flow.
+
+      Our system meticulously identifies anomalies by analyzing various factors such as luggage size, shape, and even extended handles that deviate from the norm. Leveraging edge machine learning, our platform processes data in real-time, ensuring immediate detection and response to any irregularities. By integrating advanced computer vision algorithms, we deliver precision and reliability that modern cargo and aviation industries demand.
+
+      With a remarkable 99% accuracy rate, we continuously calculate and analyze statistics of anomaly events, empowering our clients with actionable insights. To further enhance understanding, our platform includes intuitive SVG illustrations that visually represent the detection process, making it easier for operators to monitor and maintain optimal system performance.
+    `,
+    imgSrc: '/static/images/visiocue-project.svg',
+    href: '/projects/cargo-aviation-ai',
   },
 ]
 
 export default projectsData
-
