87ad1d63d62cd493d9c4dd742567a95830d95e5d
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2025-03-03 20:28:33 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,8 +29,7 @@ const Header = () => {
         </div>
       </Link>
       <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
-        
-      <SearchButton />
+        <SearchButton />
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
           {headerNavLinks
             .filter((link) => link.href !== '/')
