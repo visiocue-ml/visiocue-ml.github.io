9f1b91d6073d3012360cf8916515dd775230a047
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2025-03-03 18:45:40 +0000

    new header jsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,15 +32,25 @@ const Header = () => {
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
           {headerNavLinks
             .filter((link) => link.href !== '/')
-            .map((link) => (
-              <Link
-                key={link.title}
-                href={link.href}
-                className="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100"
-              >
-                {link.title}
-              </Link>
-            ))}
+            .map((link) =>
+              link.href === 'https://visiocue.com' ? (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="rounded-full bg-primary-500 text-white px-4 py-2 hover:bg-primary-600 transition-colors shadow-sm"
+                >
+                  {link.title}
+                </Link>
+              ) : (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100"
+                >
+                  {link.title}
+                </Link>
+              )
+            )}
         </div>
         <SearchButton />
         <MobileNav />
